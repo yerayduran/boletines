0a6889f29bbaf2900389d98d65a3876b780e67bb
yerayduran <yerayduranchaves2006@gmail.com>
2025-09-26 17:45:23 +0200

    yeray

---
+++ boletines/EjercicioBucleDo-While/src/Contraseña.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Contraseña {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String contrasenaCorrecta = "1DAM";  //es la contraseña correcta y la clase que estamos
+        String contrasenaIngresada;
+
+        do {
+            System.out.print("Introduce la contraseña: ");
+            contrasenaIngresada = scanner.nextLine();
+        } while (!contrasenaIngresada.equals(contrasenaCorrecta));
+
+        System.out.println("¡Contraseña válida! Bienvenido.");
+        scanner.close();
+    }
+}
