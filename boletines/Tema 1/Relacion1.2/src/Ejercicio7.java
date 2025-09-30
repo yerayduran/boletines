ed7459c1adedb6fd9aae224d2e4bf801b7688a9d
yerayduran <yerayduranchaves2006@gmail.com>
2025-09-30 11:13:24 +0200

    yeray

--- boletines/Tema 1/Relacion1.2/src/Ejercicio7.java
+++ boletines/Tema 1/Relacion1.2/src/Ejercicio7.java
@@ -5,12 +5,14 @@ public class Ejercicio7{
         Scanner scanner = new Scanner(System.in);
         int cantidad;
 
+
         do {
             System.out.print("¿Cuántos números deseas introducir? ");
             cantidad = scanner.nextInt();
         } while (cantidad <= 0);
 
         int suma = 0;
+
         for (int i = 1; i <= cantidad; i++) {
             System.out.print("Número " + i + ": ");
             suma += scanner.nextInt();
