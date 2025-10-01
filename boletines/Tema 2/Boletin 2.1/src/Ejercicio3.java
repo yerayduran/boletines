75e1de031329c91288f4d2b557b77ee610fcda57
yerayduran <yerayduranchaves2006@gmail.com>
2025-10-01 15:47:43 +0200

    yeray

--- boletines/Tema 2/Boletin 2.1/src/Ejercicio3.java
+++ boletines/Tema 2/Boletin 2.1/src/Ejercicio3.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Introduce un número: ");
+        int numero = scanner.nextInt();
+
+
+        numero = Math.abs(numero);
+
+
+        int cifras = (numero == 0) ? 1 : (int) Math.log10(numero) + 1;
+
+
+        System.out.println("El número " + numero + " tiene " + cifras + " cifras.");
+    }
+}
